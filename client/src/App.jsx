a2542328cf3fe7aa2cd576de7a918566147ce3f0
YohanNayanajith <yohannayanajith40@gmail.com>
2022-07-16 04:45:08 +0530

    After dashboard UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Dashboard from "./components/Dashboard/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInSide from "./pages/SignIn";
 
@@ -7,7 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SignInSide />} />
         <Route index element={<SignInSide />} />
-        {/* <Route path="blogs" element={<Blogs />} /> */}
+        <Route path="dashboard" element={<Dashboard />} />
         {/* <Route path="contact" element={<Contact />} /> */}
         {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -10,8 +10,10 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import GoogleIcon from "@mui/icons-material/Google";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Divider } from "@mui/material";
 
 function Copyright(props) {
   return (
@@ -116,6 +118,16 @@ export default function SignInSide() {
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
+              </Button>
+              <Divider>or</Divider>
+              <Button
+                variant="outlined"
+                fullWidth
+                sx={{ mt: 3, mb: 2, color:"#378CBB" }}
+                // color="success"
+                startIcon={<GoogleIcon />}
+              >
+                Continue with google
               </Button>
               <Grid container>
                 <Grid item xs>
