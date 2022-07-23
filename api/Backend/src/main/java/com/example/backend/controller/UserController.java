42a04db109e37c9d2b486e2ebca7c8289bd5ead4
YohanNayanajith <yohannayanajith40@gmail.com>
2022-07-23 09:04:58 +0530

    Second commit

---
+++ api/Backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.AppUser;
+import com.example.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<AppUser>>getUsers(){
+        return ResponseEntity.ok().body(userService.getUser());
+    }
+}
---
+++ api/Backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.backend.service;
+
+import com.example.backend.model.AppUser;
+import com.example.backend.model.UserRole;
+
+import java.util.List;
+
+public interface UserService {
+    AppUser saveUser(AppUser appUser);
+    UserRole saveRole(UserRole userRole);
+    void addRoleToUser(String username, String roleName);
+    AppUser getUser(String username);
+    List<AppUser> getUser();
+}
