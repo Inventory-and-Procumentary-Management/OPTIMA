cdf987502db237efb4f12fd807c5e910b129a840
YohanNayanajith <yohannayanajith40@gmail.com>
2022-08-01 23:21:46 +0530

    new one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Dashboard from "./components/Dashboard/Dashboard";
+import Purchase from "./pages/PurchasingManager/Purchase";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInSide from "./pages/SignIn";
 
@@ -9,6 +10,7 @@ const App = () => {
         <Route path="/" element={<SignInSide />} />
         <Route index element={<SignInSide />} />
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="purchase" element={<Purchase />} />
         {/* <Route path="contact" element={<Contact />} /> */}
         {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
